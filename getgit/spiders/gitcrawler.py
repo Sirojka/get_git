5b19b78c1fe855a173788909091a68b1820bca15
Siarhei Straltsou <straltsou.siarhei@gmail.com>
2022-07-14 23:46:27 +0300

    add new flow, account link -> list of repo -> info about repo

--- getgit/spiders/gitcrawler.py
+++ getgit/spiders/gitcrawler.py
@@ -45,9 +45,9 @@ class GitSpider(scrapy.Spider):
             if len(url_path_chunks) == 1:
                 api_url = 'https://api.github.com/users/{}/repos'.format(url_path_chunks[0])
             else:
-                self.logger.error('Bad link: {}'.format(link))
+                self.logger.error('Bad link: {}, not an account link, maybe direct repo link'.format(link))
         else:
-            self.logger.error('Bad link: {}'.format(link))
+            self.logger.error('Bad link: {}, not a github site'.format(link))
         return api_url
 
     def start_requests(self):
@@ -64,72 +64,59 @@ class GitSpider(scrapy.Spider):
 
     def parse(self, response, **kwargs):
         resp_data = response.json()
-        user = None
+        owner = None
         if len(resp_data) > 0:
             for repo in resp_data:
                 item = GitItem()
                 item['full_name'] = repo.get('full_name')
+                self.logger.info('Repo: {}'.format(item['full_name']))
                 item['description'] = repo.get('description')
                 item['site_url'] = repo.get('html_url')
                 item['stars'] = repo.get('stargazers_count')
                 item['forks'] = repo.get('forks_count')
                 item['watching'] = repo.get('watchers_count')
-                user = repo.get('owner', dict()).get('login')
+                owner = repo.get('owner', dict()).get('login')
                 default_branch = repo.get('default_branch')
                 url = 'https://api.github.com/repos/{}/commits'.format(item['full_name'])
                 yield scrapy.Request(url=url, callback=self.parse_last_commit, meta={'item': item,
                                                                                      'default_branch': default_branch})
         if response.headers.get('Link'):
-            next_page = response.headers.get('Link').decode().split(',')[0]
-            if 'next' in next_page:
-                next_page_url = next_page.split(';')[0].split('<')[1].split('>')[0]
-                next_page_num = int(next_page_url.split('=')[1])
-                url = 'https://api.github.com/users/{}/repos?page={}'.format(user, next_page_num)
-                yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse_repos(self, response, **kwargs):
-        item = response.meta.get('item')
-        owner = response.meta.get('owner')
-        resp_data = response.json()
-        if len(resp_data) > 0:
-            for repo in resp_data:
-
-        self.logger.info(response.headers)
+            nav = response.headers.get('Link').decode().split(',')
+            for n in nav:
+                if 'next' in n:
+                    next_page_url = n.split(';')[0].split('<')[1].split('>')[0]
+                    next_page_num = int(next_page_url.split('=')[1])
+                    url = 'https://api.github.com/users/{}/repos?page={}'.format(owner, next_page_num)
+                    yield scrapy.Request(url=url, callback=self.parse)
 
     def parse_last_commit(self, response, **kwargs):
         item = response.meta.get('item')
         default_branch = response.meta.get('default_branch')
+        owner, repo = item.get('full_name').split('/')
         resp_data = response.json()
         last_commit = resp_data[0]
         if response.headers.get('Link'):
             page_url = response.headers.get('Link').decode().split(',')[1].split(';')[0].split('<')[1].split('>')[0]
             yield scrapy.Request(url=page_url, callback=self.parse_first_commit,
-                                 meta={'item': item, 'owner': owner, 'repo': repo, 'default_branch': default_branch,
-                                       'last_commit': last_commit})
+                                 meta={'item': item, 'default_branch': default_branch, 'last_commit': last_commit})
         else:
             first_commit_hash = resp_data[-1]['sha']
             url = 'https://api.github.com/repos/{}/{}/compare/{}...{}'.format(owner, repo, first_commit_hash,
                                                                               default_branch)
             yield scrapy.Request(url=url, callback=self.parse_all_commits_count, meta={'item': item,
-                                                                                       'owner': owner,
-                                                                                       'repo': repo,
                                                                                        'default_branch': default_branch,
                                                                                        'last_commit': last_commit})
 
     def parse_first_commit(self, response, **kwargs):
         item = response.meta.get('item')
-        owner = response.meta.get('owner')
-        repo = response.meta.get('repo')
         default_branch = response.meta.get('default_branch')
         last_commit = response.meta.get('last_commit')
+        owner, repo = item.get('full_name').split('/')
         resp_data = response.json()
         first_commit_hash = resp_data[-1]['sha']
         url = 'https://api.github.com/repos/{}/{}/compare/{}...{}'.format(owner, repo, first_commit_hash,
                                                                           default_branch)
         yield scrapy.Request(url=url, callback=self.parse_all_commits_count, meta={'item': item,
-                                                                                   'owner': owner,
-                                                                                   'repo': repo,
-                                                                                   'default_branch': default_branch,
                                                                                    'last_commit': last_commit})
 
     def parse_account_starred(self, response, **kwargs):
@@ -150,10 +137,8 @@ class GitSpider(scrapy.Spider):
 
     def parse_all_commits_count(self, response, **kwargs):
         item = response.meta.get('item')
-        owner = response.meta.get('owner')
-        repo = response.meta.get('repo')
-        default_branch = response.meta.get('default_branch')
         last_commit = response.meta.get('last_commit')
+        owner, repo = item.get('full_name').split('/')
         resp_data = response.json()
         all_commits_count = resp_data.get('total_commits') + 1
         item['commits'] = all_commits_count
