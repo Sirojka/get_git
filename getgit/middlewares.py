35864b255707ff6d3b1d7fc406fb5de54b1e5770
Siarhei Straltsou <straltsou.siarhei@gmail.com>
2022-07-15 09:51:57 +0300

    add proxy support middleware

--- getgit/middlewares.py
+++ getgit/middlewares.py
@@ -3,3 +3,10 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 # useful for handling different item types with a single interface
+
+
+class ProxiesDownloaderMiddleware(object):
+
+    def process_request(self, request, spider):
+        if spider.ext_args.proxy:
+            request.meta['proxy'] = spider.ext_args.proxy
