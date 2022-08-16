3271366f7fd58faa1089bd00b32971d94dee564f
Siarhei Straltsou <sergey.streltsov@100nuts.ai>
2022-08-16 13:29:44 +0300

    settings, version bump to 0.1.1, code cleanup, tune up autothrottle for requests with apikey, add DefaultHeadersMiddleware

--- getgit/settings.py
+++ getgit/settings.py
@@ -12,39 +12,19 @@ BOT_NAME = 'getgit'
 SPIDER_MODULES = ['getgit.spiders']
 NEWSPIDER_MODULE = 'getgit.spiders'
 
-VERSION = '0.0.8'
+VERSION = '0.1.1'
 
 LINKS_FILE_NAME = 'links.txt'
 
-# MONGO_URI = 'mongodb://localhost:27017'
 MONGO_DATABASE = 'git_get_db'
 MONGO_COLLECTION = 'git_items'
 
-# PROXY_POOL_BAN_POLICY = 'myproject.policy.BanDetectionPolicyNotText'
-
-# Obey robots.txt rules
-# ROBOTSTXT_OBEY = True
-
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 1
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 1.0
 
-DOWNLOAD_DELAY = 2.05
-
-# The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 4
-# CONCURRENT_REQUESTS_PER_IP = 4
-
-# Disable cookies (enabled by default)
 COOKIES_ENABLED = False
 
-# Disable Telnet Console (enabled by default)
-# TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -60,55 +40,28 @@ DEFAULT_REQUEST_HEADERS = {
     'Cache-Control': 'no-cache'
 }
 
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-# SPIDER_MIDDLEWARES = {
-# }
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
+    'getgit.middlewares.DefaultHeadersMiddleware': 100,
     'getgit.middlewares.ProxiesDownloaderMiddleware': 350,
     'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 400,
 }
 
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
 EXTENSIONS = {
     'getgit.extensions.SpiderLinksLoader': 100,
     'scrapy.extensions.telnet.TelnetConsole': None,
 }
 
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'getgit.pipelines.MongoPipeline': 500,
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
-
-# The initial download delay
-# AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-# AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-
-# Enable showing throttling stats for every response received:
-# AUTOTHROTTLE_DEBUG = False
+AUTOTHROTTLE_START_DELAY = 5
+AUTOTHROTTLE_MAX_DELAY = 2
+AUTOTHROTTLE_TARGET_CONCURRENCY = 0.5
+CONCURRENT_REQUESTS_PER_DOMAIN = 1
+CONCURRENT_REQUESTS_PER_IP = 1
 
 RETRY_ENABLED = True
-RETRY_TIMES = 3  # initial response + 2 retries = 3 requests
+RETRY_TIMES = 3
 RETRY_HTTP_CODES = [500, 502, 503, 504, 522, 524, 408, 429, 403, 400]
-
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-# HTTPCACHE_ENABLED = True
-# HTTPCACHE_EXPIRATION_SECS = 0
-# HTTPCACHE_DIR = 'httpcache'
-# HTTPCACHE_IGNORE_HTTP_CODES = []
-# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
